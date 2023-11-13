1646adfdaff23dd4f9685664aea9f460bc310347
aeross <aerosstube@gmail.com>
2023-11-13 19:49:12 +0300

    first commit

---
+++ custom/mappings.lua
@@ -0,0 +1,97 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    ["<leader>et"] = {"<cmd> NvimTreeToggle <CR>", "Toggle nvimtree"},
+    ["<leader>w"] = { "<cmd>w<CR>", "Save" },
+    ["<leader>ll"] = { "<cmd>TroubleToggle<CR>", "Open Trouble" },
+    ["<leader>ft"] = { "<cmd>TodoTelescope<CR>", "Find Todo" },
+    ["\\"] = { "<cmd>:vsplit <CR>", "Vertical split" },
+    ["<C-]>"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+    },
+    ["<C-\\>"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+    },
+    ["<leader>tt"] = {
+      function()
+        require("neotest").run.run()
+      end,
+      "Run nearest test",
+    },
+    ["<leader>tf"] = {
+      function()
+        require("neotest").run.run(vim.fn.expand "%")
+      end,
+      "Run file test",
+    },
+    ["<leader>to"] = {
+      ":Neotest output<CR>",
+      "Test output",
+    },
+    ["<leader>ts"] = {
+      ":Neotest summary<CR>",
+      "Test summary",
+    },
+    ["<leader>du"] = {
+      function()
+        require("dapui").toggle()
+      end,
+      "Dedug UI",
+    },
+    ["<leader>db"] = {
+      function()
+        require("dap").toggle_breakpoint()
+      end,
+      "Breakpoint",
+    },
+    ["<leader>ds"] = {
+      function()
+        require("dap").continue()
+      end,
+      "Start",
+    },
+    ["<leader>dn"] = {
+      function()
+        require("dap").step_over()
+      end,
+      "Step over",
+    },
+    ["<leader>gl"] = {
+      ":Flog<CR>",
+      "Git Log",
+    },
+    ["<leader>gf"] = {
+      ":DiffviewFileHistory<CR>",
+      "Git File History",
+    },
+    ["<leader>gc"] = {
+      ":DiffviewOpen HEAD~2<CR>",
+      "Git Last Commit",
+    },
+    ["<leader>gt"] = {
+      ":DiffviewToggleFile<CR>",
+      "Git Last Commit",
+    },
+  },
+  t = {
+    ["<C-]>"] = {
+      function()
+        require("nvterm.terminal").toggle "vertical"
+      end,
+    },
+    ["<C-\\>"] = {
+      function()
+        require("nvterm.terminal").toggle "horizontal"
+      end,
+    },
+  },
+}
+-- more keybinds!
+
+return M
